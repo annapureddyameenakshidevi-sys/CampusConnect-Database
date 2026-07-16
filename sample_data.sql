f8b678d5d0ccbfddcb3f53f72f998957760f3de0
annapureddyameenakshidevi-sys <annapureddyameenakshidevi@gmail.com>
2026-07-16 15:04:28 +0530

    Update sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -1,1 +1,11 @@
-
+INSERT INTO Students (student_id, student_name, email, department) VALUES
+(1, 'Asha', 'asha@example.com', 'CSE'),
+(2, 'Rahul', 'rahul@example.com', 'ECE'),
+(3, 'Priya', 'priya@example.com', 'EEE'),
+(4, 'Kiran', 'kiran@example.com', 'ME'),
+(5, 'Neha', 'neha@example.com', 'CSE'),
+(6, 'Arjun', 'arjun@example.com', 'ECE'),
+(7, 'Divya', 'divya@example.com', 'IT'),
+(8, 'Ravi', 'ravi@example.com', 'CIVIL'),
+(9, 'Sneha', 'sneha@example.com', 'IT'),
+(10, 'Vijay', 'vijay@example.com', 'ME');
