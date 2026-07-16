5677b27389b002027063d4ba919ecea94be3bcdd
annapureddyameenakshidevi-sys <annapureddyameenakshidevi@gmail.com>
2026-07-16 14:58:12 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE Students (
+    student_id INTEGER PRIMARY KEY,
+    student_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    department TEXT NOT NULL
+);
+
 
